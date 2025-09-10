217236b1f702b0d98e16bf6ba75fdd8cf839fbc7
biversen04 <brittany.iversen@gmail.com>
2025-09-10 11:51:20 -0400

    student view

--- src/components/ReceivedKudos.js
+++ src/components/ReceivedKudos.js
@@ -1,14 +1,52 @@
-import React from 'react';
+import React from "react";
+
+const received = [
+    {
+        sender: "Bill Gates",
+        title: "Totally Awesome!",
+        message:
+            "You are the smartest person I know. Would you mind tutoring me in Co...",
+        imageUrl: "/img/logo192.png",
+    },
+];
 
 function ReceivedKudos() {
-  return (
-    <section className="received-kudos">
-      <h3>Received Kudos</h3>
-      <div className="kudos-box">
-        {/* Received Kudos content goes here */}
-      </div>
-    </section>
-  );
+    const openImage = (url) => {
+        window.open(url, "_blank", "noopener,noreferrer");
+    };
+
+    return (
+        <section>
+            <h2>Received Kudos</h2>
+            <table className="k-table">
+                <thead>
+                <tr>
+                    <th>Sender</th>
+                    <th>Title</th>
+                    <th>Message</th>
+                </tr>
+                </thead>
+                <tbody>
+                {received.map((k, i) => (
+                    <tr
+                        key={i}
+                        className="row-click"
+                        role="button"
+                        tabIndex={0}
+                        onClick={() => openImage(k.imageUrl)}
+                        onKeyDown={(e) => {
+                            if (e.key === "Enter" || e.key === " ") openImage(k.imageUrl);
+                        }}
+                    >
+                        <td><strong>{k.sender}</strong></td>
+                        <td>{k.title}</td>
+                        <td className="truncate">{k.message}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        </section>
+    );
 }
 
 export default ReceivedKudos;
--- src/components/SentKudos.js
+++ src/components/SentKudos.js
@@ -1,19 +1,51 @@
 import React from "react";
 
+const sent = [
+    {
+        recipient: "Abraham Lincoln",
+        title: "Fantastic Effort!",
+        status: "Received",
+        imageUrl: "/img/logo192.png",
+    },
+];
+
 function SentKudos() {
-    return(
-        <section className="sent-kudos">
-            <h3>Sent Kudos</h3>
-            <div className="kudos-list-header">
-                <span>Kudos Title</span>
-                <span>Kudos Text</span>
-                <span>Kudos Status</span>
-            </div>
-            <div className="kudos-box">
-                {/* Sent Kudos content goes here */}
-            </div>
+    const openImage = (url) => {
+        window.open(url, "_blank", "noopener,noreferrer");
+    };
+
+    return (
+        <section>
+            <h2>Sent Kudos</h2>
+            <table className="k-table">
+                <thead>
+                <tr>
+                    <th>Recipient</th>
+                    <th>Title</th>
+                    <th>Kudos Status</th>
+                </tr>
+                </thead>
+                <tbody>
+                {sent.map((k, i) => (
+                    <tr
+                        key={i}
+                        className="row-click"
+                        role="button"
+                        tabIndex={0}
+                        onClick={() => openImage(k.imageUrl)}
+                        onKeyDown={(e) => {
+                            if (e.key === "Enter" || e.key === " ") openImage(k.imageUrl);
+                        }}
+                    >
+                        <td>{k.recipient}</td>
+                        <td>{k.title}</td>
+                        <td>{k.status}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
         </section>
-    )
+    );
 }
 
 export default SentKudos;
